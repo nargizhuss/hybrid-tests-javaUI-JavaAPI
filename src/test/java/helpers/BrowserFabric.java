ae912e990a9812215e071fc489942db7a7a7a3a7
nargizhuss <nargizhuss@gmail.com>
2022-01-14 14:49:23 -0500

    --headless mode added

--- src/test/java/helpers/BrowserFabric.java
+++ src/test/java/helpers/BrowserFabric.java
@@ -4,9 +4,12 @@ import enums.BrowserType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
 
 public class BrowserFabric {
 
@@ -23,17 +26,26 @@ public class BrowserFabric {
 
     private static WebDriver getChromeDriver() {
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("window-size=1400,1000");
+        return new ChromeDriver(options);
     }
 
     private static WebDriver getFireFoxDriver() {
         WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");
+        options.addArguments("--width=1400");
+        options.addArguments("--height=1000");
+        return new FirefoxDriver(options);
     }
 
     private static WebDriver getOperaDriver() {
         WebDriverManager.operadriver().setup();
-        return new OperaDriver();
+        OperaOptions options = new OperaOptions();
+        options.addArguments("--headless");
+        return new OperaDriver(options);
     }
 
     private static WebDriver getEdgeDriver() {
