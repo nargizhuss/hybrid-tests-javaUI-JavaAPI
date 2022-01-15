8569724da4e822ebc3a92b31871ccd600dc51f5c
nargizhuss <nargizhuss@gmail.com>
2022-01-15 17:50:22 -0500

    Listeners. Minor refactoring

--- src/test/java/listeners/Listeners.java
+++ src/test/java/listeners/Listeners.java
@@ -1,18 +1,29 @@
 package listeners;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+
 public class Listeners implements ITestListener {
+    private static Logger logger = LogManager.getLogger(Listeners.class);
 
     public void onTestStart(ITestResult result) {
+
+        logger.debug("Started test "+ result.getName());
+
     }
 
     public void onTestSuccess(ITestResult result) {
+
     }
 
     public void onTestFailure(ITestResult result) {
+
+        logger.error("Failed test "+result.getName());
+
     }
 
     public void onTestSkipped(ITestResult result) {
